1966e89610249e3be8c72f427178f36b23a6096e
SharaGB <shara.garcia.betancur@gmail.com>
2023-01-30 18:22:00 -0500

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """ Async generator """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
