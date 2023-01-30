28d70850c757b6a902829a45ea4fc77398fb4e40
SharaGB <shara.garcia.betancur@gmail.com>
2023-01-30 18:20:25 -0500

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Asyncio comprehension"""
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
