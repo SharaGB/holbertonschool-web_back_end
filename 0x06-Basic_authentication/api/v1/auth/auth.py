694f8168cf479f3ece371bf4ca63fb982638cb18
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-10 18:13:48 -0500

    Define which routes don't need authentication

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,46 +1,41 @@
 #!/usr/bin/env python3
 """
-Auth class
+auth
 """
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth():
+class Auth:
     """
-    auth class description
+    Auth class Doc
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        require auth function
-        Define which routes don't need authentication
-        """
-        if path is None:
+        '''
+            Define which routes don't need authentication
+        '''
+        if excluded_paths is None or excluded_paths == []:
             return True
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        for excluded_path in excluded_paths:
-            if excluded_path.endswith("*"):
-                if path.startswith(excluded_path[:-1]):
-                    return False
-            elif path == excluded_path:
+        for ex_path in excluded_paths:
+            if ex_path == path:
                 return False
+            if ex_path.endswith('*'):
+                pfx_path = ex_path[:-1]
+                if pfx_path in path:
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """
-        authorization_header function
+        Request validation!
         """
-        if request is None:
+        if request is None or ("Authorization" not in request.headers):
             return None
-        if "Authorization" not in request.headers:
-            return None
+
         return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        get current user function
-        """
-
+        nyes"""
         return None
