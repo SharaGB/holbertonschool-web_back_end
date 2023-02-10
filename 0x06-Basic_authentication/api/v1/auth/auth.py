3d73ec2096b3e56dfcf26575ab20651b8cf254f5
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-10 17:47:46 -0500

    Auth class

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -25,9 +25,9 @@ class Auth():
     def authorization_header(self, request=None) -> str:
         """ Return the header value
         """
-        if request is None or ("Authorization" not in request.headers):
+        if request is None or 'Authorization' not in request.headers:
             return None
-        return request.headers["Authorization"]
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return the current user
