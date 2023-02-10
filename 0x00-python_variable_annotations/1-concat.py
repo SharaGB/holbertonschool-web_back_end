d45d7eb751eb8885c00150c27512bb40f1e93388
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-10 17:06:01 -0500

    Cache cleaning

--- 0x00-python_variable_annotations/1-concat.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" Type-annotated function """
-
-
-def concat(str1: str, str2: str) -> str:
-    """ Type-annotated function concat that takes a string str1 and a
-        string str2 as arguments and returns a concatenated string.
-    """
-    return f'{str1}{str2}'
