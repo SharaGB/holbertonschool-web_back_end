dabdc0e0a4e5ebbd2229b094d064368f44f5b6f1
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-10 17:36:04 -0500

    Auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determine if the path is in the excluded paths
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        for excluded_path in excluded_paths:
+            if excluded_path == path:
+                return False
+            if excluded_path.endswith('*') and path.startswith(excluded_path[:-1]):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Return the header value
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Return the current user
+        """
+        return None
