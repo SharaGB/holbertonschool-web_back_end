8ae2251d535c2111741e6a3a26ba59d682010fa6
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-10 19:35:16 -0500

    Basic - Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 """ BasicAuth class module """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ BasicAuth class
     """
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """ Return the Base64 part of the Authorization header
             for a Basic Authentication
         """
         if authorization_header is None or \
-                type(authorization_header) is not str:
-            return None
-        if authorization_header[:6] != 'Basic ':
+                type(authorization_header) is not str \
+                or authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Return the decoded value of a Base64 string
+        """
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) is not str:
+            return None
+        try:
+            decode = base64.b64decode(base64_authorization_header)
+            return decode.decode('utf-8')
+        except Exception:
+            return None
