c5f1ebf37082eebf7791cc44f14897249c0895bf
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-10 19:24:05 -0500

    Basic - Base64 part

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,10 +7,14 @@ class BasicAuth(Auth):
     """ BasicAuth class
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """ Return the Base64 part of the Authorization header for a Basic Authentication """
-        if authorization_header is None or type(authorization_header) is not str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Return the Base64 part of the Authorization header
+            for a Basic Authentication
+        """
+        if authorization_header is None or \
+                type(authorization_header) is not str:
             return None
         if authorization_header[:6] != 'Basic ':
             return None
-        return authorization_header[6:(len(authorization_header))]
+        return authorization_header[6:]
