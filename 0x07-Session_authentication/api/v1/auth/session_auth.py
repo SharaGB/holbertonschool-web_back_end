c366d6794183ee60027493b22cb4cd5676ea6d73
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-13 17:08:27 -0500

    Returns a User instance based on a cookie value

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -27,7 +27,7 @@ class SessionAuth(Auth):
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        """ Overload current_user
+        """ Returns a User instance based on a cookie value
         """
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
