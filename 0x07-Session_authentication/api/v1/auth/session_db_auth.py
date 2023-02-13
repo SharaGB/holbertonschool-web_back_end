2f632fa2083d8f0de1ac2933b2044fce9dff4bcd
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-13 18:51:46 -0500

    Sessions in database

---
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+""" Session DB Auth class module """
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+
+
+class SessionDBAuth(SessionExpAuth):
+    """ SessionDBAuth class
+    """
+
+    def create_session(self, user_id=None):
+        """ Create and store a Session ID for a user_id
+        """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """ Returns a User ID based on a Session ID
+        """
+        if session_id is None:
+            return None
+
+        user_session = UserSession.search({'session_id': session_id})
+        if user_session is None:
+            return None
+
+        if not user_session:
+            return None
+
+        return user_session[0].user_id
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout
+        """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
+        user_session = UserSession.search({'session_id': session_id})
+        if user_session is None:
+            return False
+
+        user_session[0].remove()
+
+        return True
