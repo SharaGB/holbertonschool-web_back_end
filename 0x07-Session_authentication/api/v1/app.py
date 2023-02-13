6eff034020db5f6ce1de3d2447c37c6913bcfb85
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-13 16:56:02 -0500

    Add the URL path in the list of excluded paths

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -47,15 +47,16 @@ def forbidden(error) -> str:
 
 @app.before_request
 def before_request():
-    """Before request handler
+    """ Before request handler
     """
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', 'api/v1/auth_session/login/']
     if auth is None:
         return
-    if not auth.require_auth(request.path, ['/api/v1/status/',
-                                            '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+    if not auth.require_auth(request.path, excluded_paths=excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)
     elif auth.current_user(request) is None:
         abort(403)
