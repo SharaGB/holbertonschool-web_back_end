44f7254904699c978ca88148ddd5121ed2e8433b
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-14 16:25:05 -0500

    Basic dictionary

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" BasicCache module """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache defines:
+      - overwrite functions of BaseCaching
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
