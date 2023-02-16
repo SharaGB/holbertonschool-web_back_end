9df718004d7162ea7c2ae3be68dd034ea9fcf254
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-16 18:38:40 -0500

    Check encrypting password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,4 +5,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """ Returns a salted, hashed password, which is a byte string """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Returns True if the password is valid for the given hashed password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
