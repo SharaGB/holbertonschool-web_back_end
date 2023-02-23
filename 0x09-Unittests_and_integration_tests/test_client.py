bf85e27243d116710e600970bf1a450c7830fa38
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-22 21:25:01 -0500

    Parameterize and patch as decorators

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
-""" Test Github Org Client """
-import unittest
-from typing import Mapping, Sequence
-from parameterized import parameterized
-from unittest.mock import patch, PropertyMock
-from unittest import mock
+""" Module for testing client """
+
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+import json
+import unittest
+from unittest.mock import patch, PropertyMock, Mock
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """
-    test github org client class
-    """
+    """ Class for Testing Github Org Client """
 
-    @parameterized.expand([("google"), ("abc")])
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
     @patch('client.get_json')
-    def test_org(self, url, payload):
-        """test org class"""
-        self.assertEqual(GithubOrgClient(url).org, payload.return_value)
-        payload.assert_called_once()
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_org(self, input, mock):
+        """Test that GithubOrgClient.org returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
