2923203e0529ba182cc0a0b1ae8b859a7c3c4f00
SharaGB <shara.garcia.betancur@gmail.com>
2023-04-20 17:17:11 -0500

    Reading a file asynchronously with Node JS

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,39 +1,36 @@
 const fs = require('fs');
 
-const countStudents = (aPath) => new Promise((resolve, reject) => {
-  fs.readFile(aPath, 'utf8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-      return;
-    }
+module.exports = function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) return reject(Error('Cannot load the database'));
+      // split data and taking only list without header
+      const lines = data.split('\n').slice(1, -1);
+      // give the header of data
+      const header = data.split('\n').slice(0, 1)[0].split(',');
+      // find firstname and field index
+      const idxFn = header.findIndex((ele) => ele === 'firstname');
+      const idxFd = header.findIndex((ele) => ele === 'field');
+      // declarate two dictionaries for count each fields and store list of students
+      const fields = {};
+      const students = {};
 
-    const response = [];
-    let msg;
+      lines.forEach((line) => {
+        const list = line.split(',');
+        if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
+        fields[list[idxFd]] += 1;
+        if (!students[list[idxFd]]) students[list[idxFd]] = '';
+        students[list[idxFd]] += students[list[idxFd]] ? `, ${list[idxFn]}` : list[idxFn];
+      });
 
-    let lines = data.trim().split('\n');
-    lines = lines.slice(1, lines.length);
-
-    msg = `Number of students: ${lines.length}`;
-    console.log(msg);
-    response.push(msg);
-
-    const courses = {};
-    for (const row of lines) {
-      const student = row.split(',');
-      if (!courses[student[3]]) courses[student[3]] = [];
-      courses[student[3]].push(student[0]);
-    }
-    for (const course in courses) {
-      if (course) {
-        msg = `Number of students in ${course}: ${courses[course].length
-          }. List: ${courses[course].join(', ')}`;
-
-        console.log(msg);
-        response.push(msg);
+      console.log(`Number of students: ${lines.length}`);
+      for (const key in fields) {
+        if (Object.hasOwnProperty.call(fields, key)) {
+          const element = fields[key];
+          console.log(`Number of students in ${key}: ${element}. List: ${students[key]}`);
+        }
       }
-    }
-    resolve(response);
+      return resolve();
+    });
   });
-});
-
-module.exports = countStudents;
+};
