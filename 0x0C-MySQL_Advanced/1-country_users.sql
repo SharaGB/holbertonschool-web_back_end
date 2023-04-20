7053feb2441554ed80a75f8ebaeb87a204182c09
SharaGB <shara.garcia.betancur@gmail.com>
2023-04-20 09:22:05 -0500

    Creates a table users

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- SQL script that creates a table users
-
 -- Script can be executed on any database
 
 CREATE TABLE
-    IF NOT EXISTS `users` (
+    IF NOT EXISTS users (
         `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         `email` VARCHAR(255) NOT NULL UNIQUE,
         `name` VARCHAR(255),
